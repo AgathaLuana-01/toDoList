e93dab0a5d4842eb8411f73f760d49c786d4dc9a
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-09 18:45:43 -0300

    style tarefas

---
+++ src/screens/components/Tarefas/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  buttonText: {
+    fontSize: 24,
+    color: "#F2F2F2",
+  },
+  form: {
+    flex: 1,
+    flexDirection: "row",
+    padding: 12,
+    marginHorizontal: 24,
+    marginVertical: 8,
+    borderRadius: 8,
+    alignItems: "center",
+    backgroundColor: "#333333",
+  },
+  textoTarefa: {
+    flex: 1,
+    color: "#F2F2F2",
+    fontSize: 14,
+    paddingHorizontal: 8,
+  },
+  imagem: {
+   marginLeft: 'auto',
+    height: 14,
+  },
+});
