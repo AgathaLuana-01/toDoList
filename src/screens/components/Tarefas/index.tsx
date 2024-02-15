915b6800367e44b737d071f7a9f4916aca46e799
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-15 18:49:28 -0300

    Co-authored-by: Pablo Henrique <pablohenrique.r.c@gmail.com>

--- src/screens/components/Tarefas/index.tsx
+++ src/screens/components/Tarefas/index.tsx
@@ -1,40 +1,50 @@
-import * as React from 'react';
+import * as React from "react";
 import { styles } from "./styles";
 import { TouchableOpacity, View, Text, Image } from "react-native";
-import { Checkbox } from 'react-native-paper';
-
+import { Checkbox } from "react-native-paper";
 
 type Tarefas = {
-    descricao: string;
-
-    removerTarefa: () => void;
-}
-
-export function Tarefa({ descricao, removerTarefa }: Tarefas) {
-    const [checked, setChecked] = React.useState(false);
-
-    return <View style={styles.form}>
-        <View style={checked ? styles.containerCheck : styles.containerCircular}>
-            <Checkbox
-                status={checked ? 'checked' : 'unchecked'}
-                onPress={() => {
-                    setChecked(!checked);
-                }}
-                color='#F2F2F2'
-                uncheckedColor='#333333'
-            />
-        </View>
-
-        <Text style={checked ? styles.textoTarefaConcluida : styles.textoTarefa}>
-            {descricao}
-        </Text>
-
-        <TouchableOpacity onPress={removerTarefa}>
-            <Image source={require('../../../img/lixeira.png')} style={styles.imagem} />
-        </TouchableOpacity>
-
-
-    </View>
-
+  tarefa: Tarefa;
+  removerTarefa: () => void;
+  setTarefas: React.Dispatch<React.SetStateAction<Tarefa[]>>;
 };
 
+export function Tarefa({ tarefa, removerTarefa, setTarefas }: Tarefas) {
+  const checked = tarefa.status === "concluido";
+
+  return (
+    <View style={styles.form}>
+      <View style={checked ? styles.containerCheck : styles.containerCircular}>
+        <Checkbox
+          status={checked ? "checked" : "unchecked"}
+          onPress={() => {
+            setTarefas((tarefas) =>
+              tarefas.map((t) => {
+                if (t.nome === tarefa.nome) {
+                  return {
+                    ...t,
+                    status: checked ? "pendente" : "concluido",
+                  };
+                }
+                return t;
+              })
+            );
+          }}
+          color="#F2F2F2"
+          uncheckedColor="#333333"
+        />
+      </View>
+
+      <Text style={checked ? styles.textoTarefaConcluida : styles.textoTarefa}>
+        {tarefa.nome}
+      </Text>
+
+      <TouchableOpacity onPress={removerTarefa}>
+        <Image
+          source={require("../../../img/lixeira.png")}
+          style={styles.imagem}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
