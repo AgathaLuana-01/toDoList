b35e9cc2ef64648ff3ae57e8c61ec5201fa05b3b
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-15 15:33:10 -0300

    inseri o checkbox

--- src/screens/components/Tarefas/index.tsx
+++ src/screens/components/Tarefas/index.tsx
@@ -1,28 +1,40 @@
-import React from "react";
+import * as React from 'react';
 import { styles } from "./styles";
 import { TouchableOpacity, View, Text, Image } from "react-native";
+import { Checkbox } from 'react-native-paper';
+
 
 type Tarefas = {
     descricao: string;
-    concluir?: () => void;
+
     removerTarefa: () => void;
 }
 
 export function Tarefa({ descricao, removerTarefa }: Tarefas) {
-    return <View style={styles.form}>
-        <Text style={styles.buttonText}>
-            o
-        </Text>
+    const [checked, setChecked] = React.useState(false);
 
-        <Text style={styles.textoTarefa}>
+    return <View style={styles.form}>
+        <View style={checked ? styles.containerCheck : styles.containerCircular}>
+            <Checkbox
+                status={checked ? 'checked' : 'unchecked'}
+                onPress={() => {
+                    setChecked(!checked);
+                }}
+                color='#F2F2F2'
+                uncheckedColor='#333333'
+            />
+        </View>
+
+        <Text style={checked ? styles.textoTarefaConcluida : styles.textoTarefa}>
             {descricao}
         </Text>
 
         <TouchableOpacity onPress={removerTarefa}>
-            <Image source={require('../../../img/lixeira.png')} style={styles.imagem}/>
+            <Image source={require('../../../img/lixeira.png')} style={styles.imagem} />
         </TouchableOpacity>
 
 
     </View>
 
 };
+
